abba179d8aa09dc09307b8c49e8efcad9dedb0a7
MyStepfather <ilya.golyashov@yandex.ru>
2024-06-26 14:09:17 +0300

    username

--- script.js
+++ script.js
@@ -2,30 +2,30 @@ const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
 const TelegramApi = require('node-telegram-bot-api');
-const { send } = require('process');
-const token = '5980630603:AAH3ikiRkcAP3qVpKjZA9mfh1IC95pHGxVk';/*   '5902147445:AAHWNw8KGhaH3zFnDVm9iw8ETqTeWgg6inQ'; */
+const token = '5980630603:AAH3ikiRkcAP3qVpKjZA9mfh1IC95pHGxVk';
+// test token
+// const token = '5949552471:AAHrmzTqKYzuy-ihVtm6Zy43-A-K-xovAts';
 const bot = new TelegramApi(token, {polling: true});
 const GROUP_CHAT_ID = '-1001961186421';
-// const TEST_GROUP_CHAT_ID = '-740721555';
+const TEST_GROUP_CHAT_ID = '-740721555';
 let users = {};
-// let userLog = {};
-messageText = {};
+let messageText = {};
 const fileLogs = path.resolve('logs.txt');
 
-function checkInternetConnection() {
-  axios.get('http://www.google.com', { timeout: 5000 })
-      .then(() => {
-
-      })
-      .catch(() => {
-          bot.stopPolling();
-          setTimeout(() => {
-              bot.startPolling();
-          }, 5000);
-      });
-}
-
-setInterval(checkInternetConnection, 5000);
+// function checkInternetConnection() {
+//   axios.get('http://www.google.com', { timeout: 5000 })
+//       .then(() => {
+//
+//       })
+//       .catch(() => {
+//           bot.stopPolling();
+//           setTimeout(() => {
+//               bot.startPolling();
+//           }, 5000);
+//       });
+// }
+//
+// setInterval(checkInternetConnection, 5000);
 
 fs.readFile(path.resolve('users.json'), (err, data) => {
   if (err) throw err;
@@ -79,29 +79,15 @@ const questions = {
   ]
 }
 
-// let decline = ['/start']
-
 let userAnswers = {};
-
-// bot.onText(/\/start/, (msg) => {
-//   const chatId = msg.chat.id;
-//   if (!userAnswers[chatId]) {
-//     if (users[chatId]) {
-//       console.log(userAnswers)
-//       bot.sendMessage(chatId, `Привет, ${users[chatId]}! О чем расскажешь?`, options)
-//     } else {
-//       bot.sendMessage(chatId, 'Представься, пожалуйста! Как тебя зовут?\nПришли свои Имя и Фамилию');
-//     }  
-//   }
-// });
 
 bot.on('callback_query', (query) => {
   const chatId = query.message.chat.id;
-
-  if (users[chatId]) {
+  const username = query.from.username;
+  if (users[username]) {
     if (questions.hasOwnProperty(query.data)) {
       userAnswers[chatId] = {date: null, userName: null, category: query.data, answers: [], questions: [], currentQuestionIndex: 0 };
-      // let dataLog = userAnswers[chatId]; 
+      // let dataLog = userAnswers[chatId];
       askQuestion(chatId);
     }
   } else {
@@ -111,31 +97,26 @@ bot.on('callback_query', (query) => {
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
-  // console.log(msg);
+  const username = msg.from.username;
   if (msg.text === '/start') {
     if (!userAnswers[chatId]) {
-      if (users[chatId]) {
-        bot.sendMessage(chatId, `Привет, ${users[chatId]}! О чем расскажешь?`, options)
+      if (users[username]) {
+        bot.sendMessage(chatId, `Привет, ${users[username]}! О чем расскажешь?`, options)
       } else {
         bot.sendMessage(chatId, 'Представься, пожалуйста! Как тебя зовут?\nПришли свои Имя и Фамилию');
       }  
     }
   }
-  if (!users[chatId] && msg.text !== '/start') {
-
+  if (!users[username] && msg.text !== '/start') {
     if (/^[A-Za-zА-Яа-яЁё]+\s[A-Za-zА-Яа-яЁё]+$/.test(msg.text)) {
-      users[chatId] = msg.text;
-      // console.log(users);
-      // console.log(path.resolve('users.json'));
+      users[username] = msg.text;
       fs.writeFile(path.resolve('users.json'), JSON.stringify(users), (err) => {
           if (err) throw err;
       });
-      
-      console.log(`Добавлен новый пользователь ${users[chatId]}!`);
-      // appendEntry(newUserLog);
-      console.log(`Добавлен новый пользователь - ${users[chatId]}!`);
 
-      bot.sendMessage(chatId, `Привет, ${users[chatId]}!\nВыбери команду:`, options)
+      console.log(`Добавлен новый пользователь ${users[username]}!`);
+
+      bot.sendMessage(chatId, `Привет, ${users[username]}!\nВыбери команду:`, options)
     } else {
         bot.sendMessage(chatId, `Просьба ввести Имя и Фамилию`)
     }
@@ -143,13 +124,12 @@ bot.on('message', (msg) => {
   if (userAnswers.hasOwnProperty(chatId)) {
     if (msg.text && msg.text !== '/start') {
       userAnswers[chatId].date = getDate(msg.date*1000);
-      userAnswers[chatId].userName = users[chatId];
+      userAnswers[chatId].userName = users[username];
       userAnswers[chatId].answers.push(msg.text);
-      // userAnswers[chatId].questions = [];
       userAnswers[chatId].questions.push(questions[userAnswers[chatId].category][userAnswers[chatId].currentQuestionIndex]);
       userAnswers[chatId].currentQuestionIndex++;
 
-      
+
     } else {
         bot.sendMessage(chatId, `Просьба текстом`);
         switch (userAnswers[chatId].currentQuestionIndex) {
@@ -165,56 +145,52 @@ bot.on('message', (msg) => {
         }
       }
   }
-  
-  if (userAnswers[chatId].currentQuestionIndex < questions[userAnswers[chatId].category].length) {
-    askQuestion(chatId);
-  } else {
-    // console.log(userAnswers[chatId].currentQuestionIndex < questions[userAnswers[chatId].category].length)
-    let finalMessage;
+  if (userAnswers.hasOwnProperty(chatId) && userAnswers[chatId] !== undefined) {
+    if (userAnswers[chatId] && userAnswers[chatId].currentQuestionIndex < questions[userAnswers[chatId].category].length) {
+      askQuestion(chatId);
+    } else {
+      let finalMessage;
+      switch (userAnswers[chatId].category) {
+        case "Буду позже":
+          finalMessage = `<b>${userAnswers[chatId].category}</b>\n${String.fromCodePoint(0x1F464)} Имя - ${users[username]}\n${String.fromCodePoint(0x23F0)} Будет в офисе - ${userAnswers[chatId].answers[0]}\n${String.fromCodePoint(0x2753)} Причина - ${userAnswers[chatId].answers[1]}\n@${username}`;
+          break;
+        case "Опаздываю":
+          finalMessage = `<b>${userAnswers[chatId].category}</b>\n${String.fromCodePoint(0x1F464)} Имя - ${users[username]}\n${String.fromCodePoint(0x23F0)} Опаздывает на - ${userAnswers[chatId].answers[0]}\n${String.fromCodePoint(0x2753)} Причина - ${userAnswers[chatId].answers[1]}\n@${username}`;
+          break
+        case "Заболел":
+          finalMessage = `<b>${userAnswers[chatId].category}</b>\n${String.fromCodePoint(0x1F464)} Имя - ${users[username]}\n${String.fromCodePoint(0x23F0)} Самочувствие - ${userAnswers[chatId].answers[0]}\n@${username}`;
+          break
+        case "На удаленке":
+          finalMessage = `<b>${userAnswers[chatId].category}</b>\n${String.fromCodePoint(0x1F464)} Имя - ${users[username]}\n${String.fromCodePoint(0x23F0)} Дней на удаленке - ${userAnswers[chatId].answers[0]}\n@${username}`;
+          break
+        case "Командировка":
+          finalMessage = `<b>${userAnswers[chatId].category}</b>\n${String.fromCodePoint(0x1F464)} Имя - ${users[username]}\n${String.fromCodePoint(0x23F0)} Даты командировки - ${userAnswers[chatId].answers[0]}\n@${username}`;
+          break
+        case "В отпуске":
+          finalMessage = `<b>${userAnswers[chatId].category}</b>\n${String.fromCodePoint(0x1F464)} Имя - ${users[username]}\n${String.fromCodePoint(0x23F0)} Даты отпуска - ${userAnswers[chatId].answers[0]}\n@${username}`;
+          break
+        case "День за свой счет":
+          finalMessage = `<b>${userAnswers[chatId].category}</b>\n${String.fromCodePoint(0x1F464)} Имя - ${users[username]}\n${String.fromCodePoint(0x23F0)} Дней взял - ${userAnswers[chatId].answers[0]}\n@${username}`;
+          break
+      }
 
-    
-    switch (userAnswers[chatId].category) {
-      case "Буду позже":
-        finalMessage = `<b>${userAnswers[chatId].category}</b>\n${String.fromCodePoint(0x1F464)} Имя - ${users[chatId]}\n${String.fromCodePoint(0x23F0)} Будет в офисе - ${userAnswers[chatId].answers[0]}\n${String.fromCodePoint(0x2753)} Причина - ${userAnswers[chatId].answers[1]}`;
-        break;
-      case "Опаздываю":
-        finalMessage = `<b>${userAnswers[chatId].category}</b>\n${String.fromCodePoint(0x1F464)} Имя - ${users[chatId]}\n${String.fromCodePoint(0x23F0)} Опаздывает на - ${userAnswers[chatId].answers[0]}\n${String.fromCodePoint(0x2753)} Причина - ${userAnswers[chatId].answers[1]}`;
-        break
-      case "Заболел":
-        finalMessage = `<b>${userAnswers[chatId].category}</b>\n${String.fromCodePoint(0x1F464)} Имя - ${users[chatId]}\n${String.fromCodePoint(0x23F0)} Самочувствие - ${userAnswers[chatId].answers[0]}`;
-        break
-      case "На удаленке":
-        finalMessage = `<b>${userAnswers[chatId].category}</b>\n${String.fromCodePoint(0x1F464)} Имя - ${users[chatId]}\n${String.fromCodePoint(0x23F0)} Дней на удаленке - ${userAnswers[chatId].answers[0]}`;
-        break
-      case "Командировка":
-        finalMessage = `<b>${userAnswers[chatId].category}</b>\n${String.fromCodePoint(0x1F464)} Имя - ${users[chatId]}\n${String.fromCodePoint(0x23F0)} Даты командировки - ${userAnswers[chatId].answers[0]}`;
-        break
-      case "В отпуске":
-        finalMessage = `<b>${userAnswers[chatId].category}</b>\n${String.fromCodePoint(0x1F464)} Имя - ${users[chatId]}\n${String.fromCodePoint(0x23F0)} Даты отпуска - ${userAnswers[chatId].answers[0]}`;
-        break
-      case "День за свой счет":
-        finalMessage = `<b>${userAnswers[chatId].category}</b>\n${String.fromCodePoint(0x1F464)} Имя - ${users[chatId]}\n${String.fromCodePoint(0x23F0)} Дней взял - ${userAnswers[chatId].answers[0]}`;
-        break
+      bot.sendMessage(chatId, 'Мы передали твой ответ руководству. Ждем в офисе ' + String.fromCodePoint(0x1F49B));
+      setTimeout(() => {
+        bot.sendMessage(chatId, 'Мы записали твой прошлый ответ. Это окно с выбором понадобится тебе в следующий раз ' + String.fromCodePoint(0x1FAF6), options);
+      }, 2000);
+      try {
+        bot.sendMessage(GROUP_CHAT_ID, finalMessage, {parse_mode: 'HTML'});
+        // bot.sendMessage(TEST_GROUP_CHAT_ID, finalMessage, {parse_mode: 'HTML'});
+        writeLog(chatId);
+
+      } catch (error) {
+        bot.sendMessage(GROUP_CHAT_ID, 'Произошла ошибка, пожалуйста, повтори отправку позже!', {parse_mode: 'HTML'});
+        // bot.sendMessage(TEST_GROUP_CHAT_ID, 'Произошла ошибка, пожалуйста, повтори отправку позже!', {parse_mode: 'HTML'});
+        console.log(error);
+      }
     }
-
-    bot.sendMessage(chatId, 'Мы передали твой ответ Алене Костиной. Ждем в офисе ' + String.fromCodePoint(0x1F49B));
-    setTimeout(() => {
-      bot.sendMessage(chatId, 'Мы записали твой прошлый ответ. Это окно с выбором понадобится тебе в следующий раз ' + String.fromCodePoint(0x1FAF6), options);
-    }, 2000);
-    try {
-      bot.sendMessage(GROUP_CHAT_ID, finalMessage, {parse_mode: 'HTML'});
-      // bot.sendMessage(TEST_GROUP_CHAT_ID, finalMessage, {parse_mode: 'HTML'});
-      writeLog(chatId);
-
-    } catch (error) {
-      bot.sendMessage(GROUP_CHAT_ID, 'Произошла ошибка, пожалуйста, повтори отправку позже!', {parse_mode: 'HTML'});
-      // bot.sendMessage(TEST_GROUP_CHAT_ID, 'Произошла ошибка, пожалуйста, повтори отправку позже!', {parse_mode: 'HTML'});
-      console.log(error);
-    }
-    // console.log(userAnswers[chatId]);
-
-
   }
+
 
 });
 
@@ -229,13 +205,12 @@ function getDate(milliseconds) {
   let date = new Date(milliseconds);
 
   let day = date.getDate();
-  let month = date.getMonth() + 1; 
+  let month = date.getMonth() + 1;
   let year = date.getFullYear();
   let hours = date.getHours();
   let minutes = date.getMinutes();
-  
-  let formattedDate = `${day}-${month}-${year}, ${hours}:${minutes}`;
-  return formattedDate;
+
+  return `${day}-${month}-${year}, ${hours}:${minutes}`;
 }
 
 let logCounter = 0;
